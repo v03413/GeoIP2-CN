f261c86b71ac5bd2515ab7500d29aacb995879ca
zhiqiang.wang <xv12@qq.com>
2021-12-15 14:10:23 +0800

    first commit

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+$data = [];
+$file = './china_ip_list.txt';
+$src  = 'https://cdn.jsdelivr.net/gh/Hackl0us/GeoIP2-CN@release/CN-ip-cidr.txt';
+$arr  = explode("\n", file_get_contents($src));
+foreach ($arr as $tmp) {
+    $line = trim($tmp);
+    if (empty($line)) {
+        continue;
+
+    }
+
+    $data[] = $line;
+}
+
+
+file_put_contents($file, implode("\n", $data));
+
+echo date('[Y-m-d H:i:s] ') . "成功更新：" . count($data);
+echo "\n";
