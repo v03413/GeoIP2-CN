e38f80734eb123fc764ccfcb1b6ded27ee79621c
zhiqiang.wang <xv12@qq.com>
2021-12-15 16:53:53 +0800

    fix path

--- main.php
+++ main.php
@@ -1,7 +1,6 @@
 <?php
 $data = [];
-$dst  = './dst/';
-$name = 'China_IP_list.txt';
+$file = sys_get_temp_dir() . '/China_IP_list.txt';
 $src  = 'https://cdn.jsdelivr.net/gh/Hackl0us/GeoIP2-CN@release/CN-ip-cidr.txt';
 $arr  = explode("\n", file_get_contents($src));
 foreach ($arr as $tmp) {
@@ -14,12 +13,7 @@ foreach ($arr as $tmp) {
     $data[] = $line;
 }
 
-if (!is_dir($dst)) {
-
-    mkdir($dst);
-}
-
-file_put_contents($dst . $name, implode("\n", $data));
-
+file_put_contents($file, implode("\n", $data));
+echo $file . "\n";
 echo date('[Y-m-d H:i:s] ') . "成功更新：" . count($data);
 echo "\n";
